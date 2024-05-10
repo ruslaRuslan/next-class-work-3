6c168ab6d7ec2559a1541b05d616132d12029980
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-05-10 12:31:29 +0400

    style verdik (xirda style)

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,14 @@
 import { Button } from "@mui/material";
-
+import styles from "./index.module.css"
 const AboutPage = () => {
-    return <div>AboutPage</div>;
-    <Button>bura klikle</Button>
-  };
-  
-  export default AboutPage;
-  
+  return (
+    <>
+      <div>AboutPage</div>
+      <Button className={styles.btn} variant="outlined">bura klikle</Button>
+      <Button variant="outlined">bura klikle</Button>
+
+    </>
+  );
+};
+
+export default AboutPage;

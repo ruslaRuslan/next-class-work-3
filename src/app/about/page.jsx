a7282b34148427076e4bc83395e2ec93fdbbabf8
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-05-10 12:39:31 +0400

    icon-nu ise saldix, ve button-da isdeyir

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,12 +1,18 @@
-import { Button } from "@mui/material";
-import styles from "./index.module.css"
+import { Button, IconButton } from "@mui/material";
+import styles from "./index.module.css";
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
+
 const AboutPage = () => {
   return (
     <>
       <div>AboutPage</div>
-      <Button className={styles.btn} variant="outlined">bura klikle</Button>
+      <IconButton>
+      <AccessAlarmIcon />
+      </IconButton>
+      <Button className={styles.btn} variant="outlined">
+        bura klikle
+      </Button>
       <Button variant="outlined">bura klikle</Button>
-
     </>
   );
 };
